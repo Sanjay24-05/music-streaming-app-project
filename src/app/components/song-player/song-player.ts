f8071961757b9560207251a3470a4edead7a2bc2
Sanjay <sanjay240506@gmail.com>
2026-01-12 22:35:17 +0530

    Created Components

---
+++ src/app/components/song-player/song-player.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-song-player',
+  imports: [],
+  templateUrl: './song-player.html',
+  styleUrl: './song-player.css',
+})
+export class SongPlayer {
+
+}
